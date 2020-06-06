8441bf7de9c8a9a8b65a9fa968809d1bb9fabcb5
abhishek k <abhi281342@gmail.com>
2020-06-06 08:23:36 +0530

    added new files

---
+++ controllers/profile.js
@@ -0,0 +1,23 @@
+const handleprofile=(req, res,db) => {
+    const { id } = req.params;
+
+    db.select('*').from('users').where({ id: id })
+        .then(user => {
+            // res.json(user[0]);
+            // console.log(user[0]);
+            if (user.length) {
+                res.json(user[0])
+            }
+            else {
+                res.status(400).json('not found')
+            }
+        })
+        .catch(err => res.status(400).json("error getting user"))
+
+
+
+};
+
+module.exports={
+    handleprofile:handleprofile
+}
---
+++ server.js
@@ -0,0 +1,56 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const bcrypt=require('bcrypt-nodejs');
+const cors=require('cors');
+const knex =require('knex');
+const register =require('./controllers/register');
+const Signin=require('./controllers/Signin');
+const profile=require('./controllers/profile');
+const image=require('./controllers/image');
+const db=knex({
+    client:'pg',
+    connection:{
+        host:'127.0.0.1',
+        user:'abhishek',
+        password:'abhi968611',
+        database:'smart-brain'
+    }
+})
+db.select('*').from('users').then(data =>{
+    // console.log(data);
+});
+const app=express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send(database.users);
+})
+
+app.post('/signin',(req,res)=> {Signin.handleSignin(req,res,db,bcrypt)})
+
+app.post('/register',(req,res)=>{register.handleRegister(req,res,db,bcrypt)})
+app.get('/profile/:id',(req,res)=>{profile.handleprofile(req,res,db)})
+app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
+
+
+
+// Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function (err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function (err, res) {
+//     // res = false
+// });
+app.listen(3001,()=>{
+    console.log('app is running');
+});
+
+/*
+/ res=this is working
+/signin --> POST=sucess/fail
+/register --> POST=user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+*/
